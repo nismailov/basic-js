158bc26c50e1a1876ed933cada9b073625589709
Nabi <ismailov.nabi2017@yandex.ru>
2023-04-16 03:46:42 +0400

    fix:what-season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -30,6 +30,30 @@ function dateSample(sampleActivity) {
     }
 }
 
+function getSeason(date) {
+    function returnSeason() {
+        let month = date.getMonth();
+        if (month === 11 || month === 0 || month === 1) {
+            return 'winter';
+        } else if (month === 2 || month === 3 || month === 4) {
+            return 'spring';
+        } else if (month === 5 || month === 6 || month === 7) {
+            return 'summer';
+        } else if (month === 8 || month === 9 || month === 10) {
+            return 'autumn'
+        }
+    }
+
+
+    if (typeof date == 'undefined') {
+        return 'Unable to determine the time of year!';
+    } else if (toString.call(date) === '[object Date]' && !Object.getOwnPropertyNames(date).length > 0) {
+        return returnSeason(date);
+    } else {
+        throw new Error('Invalid date!');
+    }
+}
+
 module.exports = {
-    dateSample
+    getSeason
 };
