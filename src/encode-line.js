10606881148efa350c7b403181fb1470534fb99f
Nabi <ismailov.nabi2017@yandex.ru>
2023-04-17 02:19:48 +0400

    fix: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+    let res = '';
+    let arr = str.split('');
+    let num = 1;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i + 1]) {
+            num += 1;
+        } else {
+            if (num > 1) {
+                res += `${num}${arr[i]}`
+            } else {
+                res += arr[i]
+            }
+            num = 1;
+        }
+    }
+    return res;
 }
 
+// console.log(encodeLine('aabbbc'));
+// console.log(encodeLine('abbcca'));
+
 module.exports = {
-  encodeLine
+    encodeLine
 };
