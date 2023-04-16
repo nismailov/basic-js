b140f607ff48fd0323f825f2b272f168c496bfe0
Nabi <ismailov.nabi2017@yandex.ru>
2023-04-16 04:02:38 +0400

    fix: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -18,6 +18,30 @@ function transform(/* arr */) {
   // remove line with error and write your code here
 }
 
+function transform(arr) {
+    let res = [];
+    if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                if (arr.length > i + 1) i++;
+                break;
+            case '--double-next':
+                if (arr.length > i + 1) res.push(arr[i + 1]);
+                break;
+            case '--discard-prev':
+                if (res.length > 0 && res[res.length - 1] === arr[i - 1]) res.pop();
+                break;
+            case '--double-prev':
+                if (arr[i - 1] && arr[i - 2] != '--discard-next') res.push(arr[i - 1]);
+                break;
+            default:
+                res.push(arr[i]);
+        }
+    }
+    return res;
+}
+
 module.exports = {
-  transform
+    transform
 };
