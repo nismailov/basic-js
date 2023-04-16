ddb22a695a5b10845a6e90df377a94a58af50511
Nabi <ismailov.nabi2017@yandex.ru>
2023-04-16 21:39:42 +0400

    fix: recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,12 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr) {
+        let depth = 1;
+        let max = 0;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                max = Math.max(this.calculateDepth(arr[i]), max);
+            }
+        }
+        return depth + max;
+    }
 }
 
 module.exports = {
-  DepthCalculator
+    DepthCalculator
 };
